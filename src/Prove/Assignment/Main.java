82840ec6bd078684522ee161f168d4174b95c507
ojorojustice <ojo19001@byui.edu>
2021-05-24 11:29:42 +0100

    I have added a line

--- src/Prove/Assignment/Main.java
+++ src/Prove/Assignment/Main.java
@@ -3,6 +3,8 @@ package Prove.Assignment;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello, my name is Benard");
+        System.out.println("Hello Benard, I trust you are fine.");
     }
 }
